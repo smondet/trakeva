f7c7f1a31b5e1a5965dee86660d1aaceb5544257
Sebastien Mondet <seb@mondet.org>
2015-04-16 22:17:39 -0400

    Add a first version of `Trakeva_cache`

---
+++ src/lib/trakeva_cache.mli
@@ -0,0 +1,22 @@
+(**************************************************************************)
+(*  Copyright 2015, Sebastien Mondet <seb@mondet.org>                     *)
+(*                                                                        *)
+(*  Licensed under the Apache License, Version 2.0 (the "License");       *)
+(*  you may not use this file except in compliance with the License.      *)
+(*  You may obtain a copy of the License at                               *)
+(*                                                                        *)
+(*      http://www.apache.org/licenses/LICENSE-2.0                        *)
+(*                                                                        *)
+(*  Unless required by applicable law or agreed to in writing, software   *)
+(*  distributed under the License is distributed on an "AS IS" BASIS,     *)
+(*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or       *)
+(*  implied.  See the License for the specific language governing         *)
+(*  permissions and limitations under the License.                        *)
+(**************************************************************************)
+
+(** Add a very greedy cache around an existing Key-value store. *)
+module Add (KV_DB: Trakeva.KEY_VALUE_STORE): Trakeva.KEY_VALUE_STORE
+
+
+val debug : bool ref
+
