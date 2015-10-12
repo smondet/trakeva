954aee621fdb94228a391325d72a4c84cc9e48b2
Leonid Rozenberg <leonidr@gmail.com>
2015-10-12 17:28:05 -0400

    Remove duplicated documentated in implementation file.

--- src/lib/trakeva.ml
+++ src/lib/trakeva.ml
@@ -73,39 +73,30 @@ end
 
 module type KEY_VALUE_STORE = sig
   type t
-  (** The handle to the database. *)
 
   val load :
     string ->
     (t, [> `Database of [> `Load of string ] * string ]) Deferred_result.t
-  (** Load a handle from the given database parameters. *)
 
   val close: t ->
     (unit, [> `Database of [> `Close ] * string ]) Deferred_result.t
-  (** Close the DB. *)
 
   val get : ?collection:string -> t -> key:string ->
     (string option, [> `Database of [> `Get of Key_in_collection.t ] * string ])
       Deferred_result.t
-  (** Get a value in the DB. *)
 
   val get_all: t -> collection:string ->
     (string list, [> `Database of [> `Get_all of string ] * string ])
       Deferred_result.t
-  (** Get all the keys in a given collection as a list. *)
 
   val iterator: t -> collection:string ->
     (unit ->
      (string option, [> `Database of [> `Iter of string ] * string ]) Deferred_result.t)
-  (** Go through all the kets in a given collection (exact semantics
-      versus concurrent writes still to be defined …). *)
 
   val act :
     t ->
     action:Action.t ->
     ([ `Done | `Not_done ],
      [> `Database of [> `Act of Action.t ] * string ]) Deferred_result.t
-  (** Process a transaction, which can be [`Done] is successful or [`Not_done]
-      if one of the checks in the [Action.t] failed. *)
 
 end
