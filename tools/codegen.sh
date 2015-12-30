2ecef0181f394ceec50a9e7064ac1617b218979c
Sebastien Mondet <seb@mondet.org>
2015-12-30 12:40:00 -0500

    Make `Trakeva_of_uri.load` really exceptionless

--- tools/codegen.sh
+++ tools/codegen.sh
@@ -50,28 +50,32 @@ type t = {
 }
 let load s =
   let uri = Uri.of_string s in
-  let backend, load_parameters =
-     match Uri.scheme uri with
-     | Some "postgresql" -> ((module Postgresql : Trakeva.KEY_VALUE_STORE), s)
-     | Some "sqlite" | None ->
-       ((module Sqlite : Trakeva.KEY_VALUE_STORE), Uri.path uri)
-     | Some other ->
-       Printf.ksprintf failwith "Can't recognize URI scheme: %S" other
-   in
-   let module KV = (val backend) in
-   KV.load load_parameters
-   >>= fun backend_handle ->
-   let module Implementation = struct
-     type i = t
-     type t = i
-     let load _ = assert false
-     let get ?collection t ~key = KV.get ?collection backend_handle ~key
-     let close t = KV.close backend_handle
-     let get_all _ ~collection = KV.get_all backend_handle ~collection
-     let iterator _ ~collection = KV.iterator backend_handle ~collection
-     let act _ ~action = KV.act backend_handle ~action
-   end in
-   return {implementation = (module Implementation)}
+  begin try
+    let backend, load_parameters =
+       match Uri.scheme uri with
+       | Some "postgresql" -> ((module Postgresql : Trakeva.KEY_VALUE_STORE), s)
+       | Some "sqlite" | None ->
+         ((module Sqlite : Trakeva.KEY_VALUE_STORE), Uri.path uri)
+       | Some other ->
+         Printf.ksprintf failwith "Can't recognize URI scheme: %S" other
+     in
+     let module KV = (val backend) in
+     KV.load load_parameters
+     >>= fun backend_handle ->
+     let module Implementation = struct
+       type i = t
+       type t = i
+       let load _ = assert false
+       let get ?collection t ~key = KV.get ?collection backend_handle ~key
+       let close t = KV.close backend_handle
+       let get_all _ ~collection = KV.get_all backend_handle ~collection
+       let iterator _ ~collection = KV.iterator backend_handle ~collection
+       let act _ ~action = KV.act backend_handle ~action
+     end in
+     return {implementation = (module Implementation)}
+  with
+  | Failure msg -> fail (\`Database (\`Load s, msg))
+  end
 
 let get ?collection t ~key = 
   let module KV = (val t.implementation) in
